3e5159f26dbbd323f7de499f275123dea51dc9ba
Yosr Gamal <yosr.elqott@gmail.com>
2021-12-28 20:34:01 +0200

    bestfit function, worstfit function and class result

--- OS2.cpp
+++ OS2.cpp
@@ -4,59 +4,206 @@
 #include <string>
 
 using namespace std;
-void erase(int x, vector<int>& p) {
 
-	vector<int>temp;
+class Result
+{
+public:
+	int block;
+	int process;
 
-	for (int i = 0; i < p.size(); i++) {
-		if (i != x) { //we will ignore the process that was placed in the freeblock vector
+	Result(int block, int process)
+	{
+		this->block = block;
+		this->process = process;
+	}
+};
+
+void erase(int x, vector<int> &p)
+{
+
+	vector<int> temp;
+
+	for (int i = 0; i < p.size(); i++)
+	{
+		if (i != x)
+		{ //we will ignore the process that was placed in the freeblock vector
 			temp.push_back(p[i]);
 		}
 	}
 
 	p.clear();
 	p = temp;
-
 }
 
-
-void firstFit(int numOfBlocks, vector<int>& block, int numOfProcesses, vector<int>& process)
+void firstFit(int numOfBlocks, vector<int> &block, int numOfProcesses, vector<int> &process)
 {
-	vector<int>freeblock;
-	for (int y = 0; y < numOfBlocks; y++) {
+	vector<int> freeblock;
+	for (int y = 0; y < numOfBlocks; y++)
+	{
 		freeblock.push_back(0);
 	}
 
-
-	for (int i = 0; i < numOfBlocks; i++) {
+	for (int i = 0; i < numOfBlocks; i++)
+	{
 		int temp = -1;
-		for (int x = 0; x < process.size(); x++) {
-			if (block[i] >= process[x]) {
+		for (int x = 0; x < process.size(); x++)
+		{
+			if (block[i] >= process[x])
+			{
 
 				freeblock[i] = process[x];
 				temp = x;
 
 				break;
 			}
-
 		}
-		if (temp > -1) {
+		if (temp > -1)
+		{
 			erase(temp, process);
 			temp = -1;
 		}
-
 	}
 	cout << "First fit: " << endl;
-	cout << "Blocks " << " " << "Processes in blocks" << endl;
-	for (int j = 0; j < freeblock.size(); j++) {
+	cout << "Blocks "
+		 << " "
+		 << "Processes in blocks" << endl;
+	for (int j = 0; j < freeblock.size(); j++)
+	{
 		cout << " " << block[j] << "   |    ";
 		cout << freeblock[j];
 		cout << endl;
 	}
-
 }
 
-void choose(int num, int numOfBlocks, vector<int>& block, int numOfProcesses, vector<int>& process)
+void bestFit(int numOfBlocks, vector<int> &block, int numOfProcesses, vector<int> &process)
+{
+
+	vector<Result> result;
+	for (int i = 0; i < numOfBlocks; i++)
+	{
+		result.push_back(Result(block[i], 0));
+	}
+
+	int indexOfCurrentBest = -1;
+	int subValueOfCurrBest = -1;
+
+	for (int i = 0; i < numOfProcesses; i++)
+	{
+
+		for (int x = 0; x < result.size(); x++)
+		{
+			if (result[x].block >= process[i] && result[x].process == 0)
+			{
+
+				if (result[x].block == process[i])
+				{
+					subValueOfCurrBest = 0;
+					indexOfCurrentBest = x;
+
+					break;
+				}
+				else
+				{
+					int subVal = result[x].block - process[i];
+					if (subValueOfCurrBest == -1 || subVal < subValueOfCurrBest)
+					{
+						subValueOfCurrBest = subVal;
+						indexOfCurrentBest = x;
+					}
+				}
+			}
+		}
+		if (indexOfCurrentBest >= 0)
+		{
+			result[indexOfCurrentBest].process = process[i]; //inserting the process in the block
+			indexOfCurrentBest = -1;
+			subValueOfCurrBest = -1;
+		}
+	}
+	int counter = 0;
+	cout << "Best fit: " << endl;
+	cout << "Blocks"
+		 << " "
+		 << "Processes in blocks" << endl;
+	for (int j = 0; j < result.size(); j++)
+	{
+		if (result[j].process == 0)
+		{
+			counter++;
+		}
+		cout << " " << result[j].block << "   |    ";
+		cout << result[j].process;
+		cout << endl;
+	}
+
+	int count = process.size() - result.size();
+	if (count > 0)
+	{
+		cout << "Number of processes is bigger than available blocks therefore there is " << count + counter << " that weren't allocated in the memory" << endl;
+		cout << "There was " << counter << " block of memory that remained empty" << endl;
+	}
+}
+
+void worstFit(int numOfBlocks, vector<int> &block, int numOfProcesses, vector<int> &process)
+{
+
+	vector<Result> result;
+	for (int i = 0; i < numOfBlocks; i++)
+	{
+		result.push_back(Result(block[i], 0));
+	}
+
+	int indexOfCurrentWorst = -1;
+	int subValueOfCurrWorst = -1;
+
+	for (int i = 0; i < numOfProcesses; i++)
+	{
+
+		for (int x = 0; x < result.size(); x++)
+		{
+			if (result[x].block >= process[i] && result[x].process == 0)
+			{
+
+				int subVal = result[x].block - process[i];
+				if (subValueOfCurrWorst == -1 || subVal > subValueOfCurrWorst)
+				{
+					subValueOfCurrWorst = subVal;
+					indexOfCurrentWorst = x;
+				}
+			}
+		}
+		if (indexOfCurrentWorst >= 0)
+		{
+			result[indexOfCurrentWorst].process = process[i];
+
+			indexOfCurrentWorst = -1;
+			subValueOfCurrWorst = -1;
+		}
+	}
+	int counter = 0;
+	cout << "Worst fit: " << endl;
+	cout << "Blocks "
+		 << " "
+		 << "Processes in blocks" << endl;
+	for (int j = 0; j < result.size(); j++)
+	{
+		if (result[j].process == 0)
+		{
+			counter++;
+		}
+		cout << " " << result[j].block << "   |    ";
+		cout << result[j].process;
+		cout << endl;
+	}
+	int count = process.size() - result.size();
+	if (count > 0)
+	{
+		cout << "Number of processes is bigger than available blocks therefore there is " << count + counter << " that weren't allocated in the memory" << endl;
+		cout << "There was " << counter << " block of memory that remained empty" << endl;
+	}
+}
+
+void choose(int num, int numOfBlocks, vector<int> &block, int numOfProcesses, vector<int> &process)
 {
 	if (num == 1)
 	{
@@ -64,11 +211,11 @@ void choose(int num, int numOfBlocks, vector<int>& block, int numOfProcesses, ve
 	}
 	else if (num == 2)
 	{
-		
+		bestFit(numOfBlocks, block, numOfProcesses, process);
 	}
 	else if (num == 3)
 	{
-		
+		worstFit(numOfBlocks, block, numOfProcesses, process);
 	}
 }
 int main()
@@ -87,8 +234,8 @@ int main()
 	cin >> numberOfBlocks;
 	cout << endl;
 
-
-	for (int i = 0; i < numberOfBlocks; i++) {
+	for (int i = 0; i < numberOfBlocks; i++)
+	{
 		cout << "Enter block size: ";
 		cin >> blockSize;
 		block.push_back(blockSize);
@@ -100,7 +247,8 @@ int main()
 	cin >> numberOfProcesses;
 	cout << endl;
 
-	for (int i = 0; i < numberOfProcesses; i++) {
+	for (int i = 0; i < numberOfProcesses; i++)
+	{
 		cout << "Enter process size: ";
 		cin >> processSize;
 		process.push_back(processSize);
@@ -115,6 +263,4 @@ int main()
 	cout << endl;
 
 	choose(allocationMethod, numberOfBlocks, block, numberOfProcesses, process);
-
-
 }
